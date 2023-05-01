b163ee3a8e7d977a462cb2ec38f98361bcf2eb10
ansel <79257300125@ya.ru>
2023-05-01 17:44:21 +0300

    Add returning from cache

--- src/mahomaps/map/TilesProvider.java
+++ src/mahomaps/map/TilesProvider.java
@@ -14,17 +14,54 @@ public class TilesProvider extends Thread {
 
 	private Vector cache = new Vector();
 
+	private final Image transPixel;
+
+	private final Object downloadLock = new Object();
+
+	private TileId next;
+
 	public TilesProvider(String lang, String localPath) {
 		if (lang == null)
 			throw new NullPointerException("Language must be non-null!");
 		this.lang = lang;
 		this.localPath = localPath;
+		transPixel = Image.createRGBImage(new int[] { 0 }, 1, 1, true);
 	}
 
 	public void run() {
 	}
 
 	public Image getTile(TileId tileId) {
+		int max = 0x1 << tileId.zoom;
+		if (tileId.y < 0)
+			return transPixel;
+		if (tileId.y >= max)
+			return transPixel;
+		int x = tileId.x;
+		while (x < 0)
+			x += max;
+		while (x >= max)
+			x -= max;
+
+		tileId = new TileId(x, tileId.y, tileId.zoom);
+
+		Image cached = tryGetFromCache(tileId);
+		if (cached != null)
+			return cached;
+
+		next = tileId;
+		downloadLock.notifyAll();
+
+		return transPixel;
+	}
+
+	private Image tryGetFromCache(TileId id) {
+		for (int i = 0; i < cache.size(); i++) {
+			TileCache tile = (TileCache) cache.elementAt(i);
+			if (tile.is(id))
+				return tile.img;
+		}
+
 		return null;
 	}
 
