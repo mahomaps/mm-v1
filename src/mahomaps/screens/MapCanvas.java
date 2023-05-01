830f331109f8a43b748566225f2aa1469d6bba05
ansel <79257300125@ya.ru>
2023-05-01 21:07:12 +0300

    Add panning & zoom

--- src/mahomaps/screens/MapCanvas.java
+++ src/mahomaps/screens/MapCanvas.java
@@ -5,12 +5,15 @@ import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Font;
 import javax.microedition.lcdui.Graphics;
-import javax.microedition.lcdui.game.GameCanvas;
 
+import mahomaps.map.Rect;
 import mahomaps.map.TileId;
 import mahomaps.map.TilesProvider;
 
-public class MapCanvas extends GameCanvas implements CommandListener {
+public class MapCanvas extends MultitouchCanvas implements CommandListener {
+
+	public final int buttonSize = 50;
+	public final int buttonMargin = 10;
 
 	private TilesProvider tiles;
 	private Command back = new Command("Назад", Command.BACK, 0);
@@ -22,8 +25,16 @@ public class MapCanvas extends GameCanvas implements CommandListener {
 
 	String[] buttons = new String[] { "geo", "-", "+" };
 
+	// STATE
+	int zoom = 0;
+	int tileX = 0;
+	int tileY = 0;
+	int xOffset = 0;
+	int yOffset = 0;
+	int startPx, startPy;
+	int lastPx, lastPy;
+
 	public MapCanvas(TilesProvider tiles) {
-		super(false);
 		this.tiles = tiles;
 		addCommand(back);
 		addCommand(routes);
@@ -34,10 +45,12 @@ public class MapCanvas extends GameCanvas implements CommandListener {
 		setCommandListener(this);
 	}
 
+	// DRAW SECTION
+
 	private void repaint(Graphics g) {
 		final int w = getWidth();
 		final int h = getHeight();
-		g.setColor(255, 255, 255);
+		g.setGrayScale(127);
 		g.fillRect(0, 0, w, h);
 		drawMap(g, w, h);
 		drawOverlay(g, w, h);
@@ -45,44 +58,137 @@ public class MapCanvas extends GameCanvas implements CommandListener {
 	}
 
 	private void drawMap(Graphics g, int w, int h) {
-		int y = 0;
-		int yi=0;
-		while(y<h) {
-			int x = 0;
-			int xi=0;
-			while(x<w) {
-				g.drawImage(tiles.getTile(new TileId(xi, yi, 0)), x, y, 0);
-				x+=256;
+		int y = yOffset;
+		int yi = tileY;
+		while (y < h) {
+			int x = xOffset;
+			int xi = tileX;
+			while (x < w) {
+				g.drawImage(tiles.getTile(new TileId(xi, yi, zoom)), x, y, 0);
+				x += 256;
 				xi++;
 			}
-			y+=256;
+			y += 256;
 			yi++;
 		}
 	}
 
 	private void drawOverlay(Graphics g, int w, int h) {
-
+		g.setColor(0);
+		g.setFont(Font.getFont(0, 0, 8));
+		g.drawString("x " + tileX + " y " + tileY + " z " + zoom, 5, 5, 0);
 	}
 
 	private void drawUi(Graphics g, int w, int h) {
-		int size = 50;
-		int margin = 10;
 		int y = h;
 		for (int i = 0; i < 3; i++) {
-			y -= size;
-			y -= margin;
+			y -= buttonSize;
+			y -= buttonMargin;
 			g.setGrayScale(220);
-			g.fillArc(w - size - margin, y, size, size, 0, 360);
+			g.fillArc(w - buttonSize - buttonMargin, y, buttonSize, buttonSize, 0, 360);
 			g.setColor(0);
 			g.setFont(Font.getFont(0, 0, Font.SIZE_LARGE));
-			g.drawString(buttons[i], w - margin - size / 2, y + size / 2 - g.getFont().getHeight() / 2,
-					Graphics.HCENTER | Graphics.TOP);
+			g.drawString(buttons[i], w - buttonMargin - buttonSize / 2,
+					y + buttonSize / 2 - g.getFont().getHeight() / 2, Graphics.HCENTER | Graphics.TOP);
 		}
 	}
+
+	// LOGIC
 
 	public void update() {
 		repaint(getGraphics());
 		flushGraphics();
+	}
+
+	public void zoomIn() {
+		if (zoom >= 18)
+			return;
+		zoom++;
+		tileX *= 2;
+		tileY *= 2;
+		xOffset *= 2;
+		yOffset *= 2;
+		if (xOffset < -255) {
+			tileX++;
+			xOffset += 256;
+		}
+		if (yOffset < -255) {
+			tileY++;
+			yOffset += 256;
+		}
+	}
+
+	public void zoomOut() {
+		if (zoom <= 0)
+			return;
+		zoom--;
+		tileX /=2;
+		tileY /=2;
+		xOffset/=2;
+		yOffset/=2;
+	}
+
+	// INPUT
+
+	protected void keyPressed(int keyCode) {
+		// TODO Auto-generated method stub
+		super.keyPressed(keyCode);
+	}
+
+	protected void keyReleased(int keyCode) {
+		// TODO Auto-generated method stub
+		super.keyReleased(keyCode);
+	}
+
+	protected void pointerPressed(int x, int y, int n) {
+		if (n == 0) {
+			startPx = x;
+			startPy = y;
+			lastPx = x;
+			lastPy = y;
+		}
+	}
+
+	protected void pointerDragged(int x, int y, int n) {
+		if (n == 0) {
+			xOffset += x - lastPx;
+			yOffset += y - lastPy;
+			lastPx = x;
+			lastPy = y;
+
+			if (xOffset > 0) {
+				tileX--;
+				xOffset -= 256;
+			}
+			if (yOffset > 0) {
+				tileY--;
+				yOffset -= 256;
+			}
+			if (xOffset < -255) {
+				tileX++;
+				xOffset += 256;
+			}
+			if (yOffset < -255) {
+				tileY++;
+				yOffset += 256;
+			}
+		}
+	}
+
+	protected void pointerReleased(int x, int y, int n) {
+		if (n != 0)
+			return;
+		int w = getWidth();
+		int h = getHeight();
+		Rect plus = new Rect(w - buttonSize - buttonMargin, h - (buttonSize + buttonMargin) * 3, buttonSize,
+				buttonSize);
+		Rect minus = new Rect(w - buttonSize - buttonMargin, h - (buttonSize + buttonMargin) * 2, buttonSize,
+				buttonSize);
+		if (plus.containsBoth(x, y, startPx, startPy)) {
+			zoomIn();
+		} else if (minus.containsBoth(x, y, startPx, startPy)) {
+			zoomOut();
+		}
 	}
 
 	public void commandAction(Command c, Displayable d) {
