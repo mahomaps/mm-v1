3f0d9a4da87b183770d1f3810b23c4b6882dc778
ansel <79257300125@ya.ru>
2023-05-16 13:29:32 +0300

    Restruct api, add cookies support and search

--- src/mahomaps/api/YmapsApi.java
+++ src/mahomaps/api/YmapsApi.java
@@ -1,87 +1,36 @@
 package mahomaps.api;
 
-import java.io.EOFException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
-import javax.microedition.io.Connector;
-import javax.microedition.io.HttpConnection;
+import org.json.me.JSONArray;
+import org.json.me.JSONException;
+import org.json.me.JSONObject;
 
-public final class YmapsApi {
+import mahomaps.map.Geopoint;
+
+public final class YmapsApi extends YmapsApiBase {
 
 	public final String key = "d81964d6-b80c-46ed-9b29-d980a45d32f9";
-	private final String tokenMark = "token\":\"";
 
 	public String token = null;
 
-	public final String GetJsUrl() {
-		return "https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=" + key;
+	public final synchronized boolean RefreshToken() {
+		token = null;
+		token = GetToken(key);
+		return token != null;
 	}
 
-	public final boolean RefreshToken() {
-		HttpConnection hc = null;
-		InputStream raw = null;
-		InputStreamReader stream = null;
-		try {
-			hc = (HttpConnection) Connector.open(GetJsUrl());
-			hc.setRequestMethod("GET");
-			hc.setRequestProperty("User-Agent",
-					"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0");
-			int r = hc.getResponseCode();
-			raw = hc.openInputStream();
-			stream = new InputStreamReader(raw, "UTF-8");
-			while (true) {
-				int c = stream.read();
-				if (c == -1)
-					throw new EOFException();
-				if (c == '"') {
-					boolean ok = true;
-					for (int i = 0; i < tokenMark.length(); i++) {
-						c = stream.read();
-						if (c == -1)
-							throw new EOFException();
-						if (tokenMark.charAt(i) == (char) c) {
-							// pass
-						} else {
-							ok = false;
-							break;
-						}
-					}
-					if (!ok)
-						continue;
-					StringBuffer buf = new StringBuffer();
-					while (true) {
-						c = stream.read();
-						if (c == -1)
-							throw new EOFException();
-						if (c == '"')
-							break;
-						buf.append((char) c);
-					}
-					token = buf.toString();
-					break;
-				}
-			}
-			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (stream != null)
-					stream.close();
-				if (raw != null)
-					raw.close();
-				if (hc != null)
-					hc.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	private final String GetSearchUrl(String text, Geopoint around, double zone) {
+		return "https://api-maps.yandex.ru/services/search/v2/?format=json&lang=ru_RU&token=" + token
+				+ "&rspn=0&results=100&origin=jsapi2SearchControl"
+				+ "&snippets=businessrating%2F1.x%2Cmasstransit%2F1.x&ask_direct=1&experimental_maxadv=200&apikey="
+				+ key + "&text=" + EncodeUrl(text) + "&ll=" + around.lat + "%2C" + around.lon + "&spn=" + zone + "%2C"
+				+ zone;
+	}
 
-		}
-
-		return false;
+	public final JSONArray Search(String text, Geopoint around, double zone) throws JSONException, IOException {
+		JSONArray j = (new JSONObject(GetUtf(GetSearchUrl(text, around, zone)))).getJSONArray("features");
+		return j;
 	}
 
 }
---
+++ src/mahomaps/api/YmapsApiBase.java
@@ -0,0 +1,197 @@
+package mahomaps.api;
+
+import java.io.ByteArrayOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.HttpConnection;
+
+abstract class YmapsApiBase {
+
+	private final String tokenMark = "token\":\"";
+
+	private final Hashtable cookies = new Hashtable();
+
+	protected final String GetToken(String key) {
+		HttpConnection hc = null;
+		InputStream raw = null;
+		InputStreamReader stream = null;
+		try {
+			hc = (HttpConnection) Connector.open("https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=" + key);
+			hc.setRequestMethod("GET");
+			hc.setRequestProperty("User-Agent",
+					"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0");
+			int r = hc.getResponseCode();
+			if (r != 200)
+				throw new IOException("Http code: " + r);
+			AcceptCookies(hc);
+			raw = hc.openInputStream();
+			stream = new InputStreamReader(raw, "UTF-8");
+			while (true) {
+				int c = stream.read();
+				if (c == -1)
+					throw new EOFException();
+				if (c == '"') {
+					boolean ok = true;
+					for (int i = 0; i < tokenMark.length(); i++) {
+						c = stream.read();
+						if (c == -1)
+							throw new EOFException();
+						if (tokenMark.charAt(i) == (char) c) {
+							// pass
+						} else {
+							ok = false;
+							break;
+						}
+					}
+					if (!ok)
+						continue;
+					StringBuffer buf = new StringBuffer();
+					while (true) {
+						c = stream.read();
+						if (c == -1)
+							throw new EOFException();
+						if (c == '"')
+							break;
+						buf.append((char) c);
+					}
+					return buf.toString();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (stream != null)
+					stream.close();
+				if (raw != null)
+					raw.close();
+				if (hc != null)
+					hc.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return null;
+	}
+
+	protected String GetUtf(String url) throws IOException {
+		System.out.println("GET " + url);
+		HttpConnection hc = (HttpConnection) Connector.open(url);
+		InputStream is = null;
+		ByteArrayOutputStream o = null;
+		try {
+			hc.setRequestMethod("GET");
+			hc.setRequestProperty("User-Agent",
+					"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0");
+			SendCookies(hc);
+			int r = hc.getResponseCode();
+			AcceptCookies(hc);
+			is = hc.openInputStream();
+			o = new ByteArrayOutputStream();
+			byte[] buf = new byte[256];
+			int len;
+			while ((len = is.read(buf)) != -1) {
+				o.write(buf, 0, len);
+			}
+			String str = new String(o.toByteArray(), "UTF-8");
+			if (r != 200)
+				throw new IOException("Code: " + r + "\n" + str);
+			return str;
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			throw new IOException(e.toString());
+		} finally {
+			if (is != null)
+				is.close();
+			if (hc != null)
+				hc.close();
+			if (o != null)
+				o.close();
+		}
+	}
+
+	private void AcceptCookies(HttpConnection hc) throws IOException {
+		for (int i = 0; i < 100; i++) {
+			String h = hc.getHeaderFieldKey(i);
+			if (h == null) {
+				if (i > 2)
+					break;
+				continue;
+			}
+			if ("Set-Cookie".equals(h)) {
+				String val = hc.getHeaderField(i);
+				if (val != null) {
+					val = val.substring(0, val.indexOf(';'));
+					int fec = val.indexOf('=');
+					String id = val.substring(0, fec);
+					String content = val.substring(fec + 1);
+					cookies.put(id, content);
+				}
+			}
+		}
+	}
+
+	private void SendCookies(HttpConnection hc) throws IOException {
+		System.out.println("Sending " + cookies.size() + " cookies");
+		StringBuffer buf = new StringBuffer(cookies.size() * 100);
+		boolean first = true;
+		Enumeration keys = cookies.keys();
+		while (keys.hasMoreElements()) {
+			Object key = keys.nextElement();
+			String val = (String) cookies.get(key);
+			if (first) {
+				first = false;
+			} else {
+				buf.append("; ");
+			}
+			buf.append(key);
+			buf.append("=");
+			buf.append(val);
+		}
+		hc.setRequestProperty("Cookie", buf.toString());
+	}
+
+	public static String EncodeUrl(String s) {
+		StringBuffer sbuf = new StringBuffer();
+		int len = s.length();
+		for (int i = 0; i < len; i++) {
+			int ch = s.charAt(i);
+			if ((65 <= ch) && (ch <= 90)) {
+				sbuf.append((char) ch);
+			} else if ((97 <= ch) && (ch <= 122)) {
+				sbuf.append((char) ch);
+			} else if ((48 <= ch) && (ch <= 57)) {
+				sbuf.append((char) ch);
+			} else if (ch == 32) {
+				sbuf.append("%20");
+			} else if ((ch == 45) || (ch == 95) || (ch == 46) || (ch == 33) || (ch == 126) || (ch == 42) || (ch == 39)
+					|| (ch == 40) || (ch == 41) || (ch == 58) || (ch == 47)) {
+				sbuf.append((char) ch);
+			} else if (ch <= 127) {
+				sbuf.append(hex(ch));
+			} else if (ch <= 2047) {
+				sbuf.append(hex(0xC0 | ch >> 6));
+				sbuf.append(hex(0x80 | ch & 0x3F));
+			} else {
+				sbuf.append(hex(0xE0 | ch >> 12));
+				sbuf.append(hex(0x80 | ch >> 6 & 0x3F));
+				sbuf.append(hex(0x80 | ch & 0x3F));
+			}
+		}
+		return sbuf.toString();
+	}
+
+	private static String hex(int ch) {
+		String x = Integer.toHexString(ch);
+		return "%" + (x.length() == 1 ? "0" : "") + x;
+	}
+}
