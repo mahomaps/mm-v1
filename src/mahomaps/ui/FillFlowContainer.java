39493e3001f0b6962525a583e5f46e31c8e296b5
ansel <79257300125@ya.ru>
2023-05-18 02:36:35 +0300

    Set flow width to max elements width

--- src/mahomaps/ui/FillFlowContainer.java
+++ src/mahomaps/ui/FillFlowContainer.java
@@ -17,12 +17,16 @@ public class FillFlowContainer extends UIComposite {
 	public void Paint(Graphics g, int x, int y, int w, int h) {
 		Enumeration e = children.elements();
 		int th = 0;
+		int mw = 0;
 		while (e.hasMoreElements()) {
 			UIElement elem = (UIElement) e.nextElement();
 			elem.Paint(g, x, y, w, elem.H);
 			y += elem.H;
 			th += elem.H;
+			if (elem.W > mw)
+				mw = elem.W;
 		}
+		W = mw;
 		H = th;
 	}
 
