5c436f92261d9f4584584929a108138305cdcec1
ansel <79257300125@ya.ru>
2023-05-19 23:06:49 +0300

    Implement settings screen

--- src/mahomaps/Settings.java
+++ src/mahomaps/Settings.java
@@ -5,7 +5,10 @@ public class Settings {
 	private Settings() {
 	}
 
-	public static boolean allowDownload = true;
-	public static boolean drawTileInfo = true;
+	public static boolean drawTileInfo = false;
 	public static int focusZoom = 16;
+	public static int geoLook = 0;
+	public static boolean showGeo = true;
+	public static boolean allowDownload = true;
+	public static int cacheMode = 0;
 }
--- src/mahomaps/screens/SettingsScreen.java
+++ src/mahomaps/screens/SettingsScreen.java
@@ -1,26 +1,79 @@
 package mahomaps.screens;
 
+import java.io.IOException;
+
+import javax.microedition.lcdui.Choice;
+import javax.microedition.lcdui.ChoiceGroup;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Form;
-import javax.microedition.lcdui.StringItem;
+import javax.microedition.lcdui.Image;
 
 import mahomaps.MahoMapsApp;
+import mahomaps.Settings;
 
 public class SettingsScreen extends Form implements CommandListener {
 
 	private Command back = new Command("Назад", Command.BACK, 0);
 
+	private ChoiceGroup focusZoom = new ChoiceGroup("Масштаб при просмотре точек", Choice.POPUP,
+			new String[] { "15", "16", "17", "18" }, null);
+	private ChoiceGroup geoLook;
+	private ChoiceGroup geoStatus = new ChoiceGroup("Статус гео", Choice.EXCLUSIVE,
+			new String[] { "Скрывать", "Показывать" }, null);
+	private ChoiceGroup tileInfo = new ChoiceGroup("Отладка тайлов", Choice.EXCLUSIVE,
+			new String[] { "Выключено", "Включено" }, null);
+	private ChoiceGroup cache = new ChoiceGroup("Сохранять тайлы", Choice.EXCLUSIVE,
+			new String[] { "В файловую", "В данные приложения", "Не сохранять" }, null);
+
 	public SettingsScreen() {
 		super("Настройки");
-		append(new StringItem("Типа настройки", "Тут ничего нет, но мы обязательно притащим"));
 		addCommand(back);
 		setCommandListener(this);
+		Image[] imgs = null;
+		try {
+			int size = 20;
+			Image sheet = Image.createImage("/geo" + size + ".png");
+			Image i1 = Image.createImage(size, size);
+			i1.getGraphics().drawImage(sheet, 0, 0, 0);
+			Image i2 = Image.createImage(size, size);
+			i2.getGraphics().drawImage(sheet, 0, -size, 0);
+			Image i3 = Image.createImage(size, size);
+			i3.getGraphics().drawImage(sheet, 0, -(size * 2), 0);
+			imgs = new Image[] { i1, i2, i3 };
+		} catch (IOException e) {
+		}
+		geoLook = new ChoiceGroup("Вид метки геопозиции", Choice.EXCLUSIVE, new String[] { "Капля", "\"Я\"", "\"Ы\"" },
+				imgs);
+		if (Settings.focusZoom < 15)
+			Settings.focusZoom = 15;
+		if (Settings.focusZoom > 18)
+			Settings.focusZoom = 18;
+		focusZoom.setSelectedIndex(Settings.focusZoom - 15, true);
+		geoLook.setSelectedIndex(Settings.geoLook, true);
+		geoStatus.setSelectedIndex(Settings.showGeo ? 1 : 0, true);
+		tileInfo.setSelectedIndex(Settings.drawTileInfo ? 1 : 0, true);
+		cache.setSelectedIndex(Settings.cacheMode, true);
+
+		append(focusZoom);
+		append(geoLook);
+		append(geoStatus);
+		append(tileInfo);
+		// append(cache);
+	}
+
+	private void Apply() {
+		Settings.focusZoom = focusZoom.getSelectedIndex() + 15;
+		Settings.geoLook = geoLook.getSelectedIndex();
+		Settings.showGeo = geoStatus.getSelectedIndex() == 1;
+		Settings.drawTileInfo = tileInfo.getSelectedIndex() == 1;
+		Settings.cacheMode = cache.getSelectedIndex();
 	}
 
 	public void commandAction(Command c, Displayable d) {
 		if (c == back) {
+			Apply();
 			MahoMapsApp.BringMenu();
 		}
 	}
