1b503254e80ab432309a09ef80a00a13da1a0fe7
ansel <79257300125@ya.ru>
2023-05-19 03:11:17 +0300

    Add routes api

--- src/mahomaps/api/YmapsApi.java
+++ src/mahomaps/api/YmapsApi.java
@@ -2,6 +2,8 @@ package mahomaps.api;
 
 import java.io.IOException;
 
+import javax.microedition.io.ConnectionNotFoundException;
+
 import org.json.me.JSONArray;
 import org.json.me.JSONException;
 import org.json.me.JSONObject;
@@ -28,9 +30,38 @@ public final class YmapsApi extends YmapsApiBase {
 				+ zone;
 	}
 
+	private final String GetRouteUrl(Geopoint a, Geopoint b, int type) {
+		String typeS = "";
+		switch (type) {
+		case ROUTE_BYFOOT:
+			typeS = "&rtt=pd";
+			break;
+		case ROUTE_AUTO:
+			break;
+		case ROUTE_TRANSPORT:
+			typeS = "&rtt=mt";
+			break;
+		default:
+			throw new IllegalArgumentException();
+		}
+		return "https://api-maps.yandex.ru/services/route/2.0/?lang=ru_RU&token=" + token + "&rll=" + a.lon + "%2C"
+				+ a.lat + "~" + b.lon + "%2C" + b.lat + "&rtm=dtr&results=1&apikey=" + key + typeS;
+	}
+
 	public final JSONArray Search(String text, Geopoint around, double zone) throws JSONException, IOException {
 		JSONArray j = (new JSONObject(GetUtf(GetSearchUrl(text, around, zone)))).getJSONArray("features");
 		return j;
 	}
 
+	public final JSONObject Route(Geopoint a, Geopoint b, int type) throws JSONException, IOException {
+		JSONArray j = (new JSONObject(GetUtf(GetRouteUrl(a, b, type)))).getJSONArray("features");
+		if (j.length() == 0)
+			throw new ConnectionNotFoundException();
+		JSONObject j1 = j.getJSONObject(0).getJSONArray("features").getJSONObject(0).getJSONObject("properties");
+		return j1;
+	}
+
+	public static final int ROUTE_BYFOOT = 1;
+	public static final int ROUTE_AUTO = 2;
+	public static final int ROUTE_TRANSPORT = 3;
 }
