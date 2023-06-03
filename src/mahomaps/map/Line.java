4156c58bed77d47ca2d90dda380022abae8b8ee5
ansel <79257300125@ya.ru>
2023-06-03 18:49:04 +0300

    Draw bold line

--- src/mahomaps/map/Line.java
+++ src/mahomaps/map/Line.java
@@ -48,16 +48,42 @@ public class Line {
 		if (map.state.zoom != forZoom)
 			Invalidate(map.state);
 
-		int px = start.GetScreenX(map.state);
-		int py = start.GetScreenY(map.state);
+		final int cw = map.getWidth();
+		final int ch = map.getHeight();
+		final int px = start.GetScreenX(map.state);
+		final int py = start.GetScreenY(map.state);
 
-		g.setColor(0xff0000);
 		for (int i = 2; i < cache.length; i += 2) {
 			int x1 = px + cache[i - 2];
 			int y1 = py + cache[i - 1];
 			int x2 = px + cache[i];
 			int y2 = py + cache[i + 1];
-			g.drawLine(x1, y1, x2, y2);
+			boolean vis1 = Math.abs(x1) < cw && Math.abs(y1) < ch;
+			boolean vis2 = Math.abs(x2) < cw && Math.abs(y2) < ch;
+			if (!vis1 && !vis2)
+				continue;
+			g.setColor(0xff0000);
+			{
+				// first point
+				// draw nothing?
+			}
+			{
+				// second point
+				g.fillRect(x2 - 1, y2 - 2, 2, 4);
+				g.fillRect(x2 - 2, y2 - 1, 4, 2);
+			}
+			{
+				// line
+				final int hor = Math.abs(x2 - x1);
+				final int ver = Math.abs(y2 - y1);
+				if (hor > ver) {
+					g.fillTriangle(x1, y1 - 2, x1, y1 + 2, x2, y2 + 2);
+					g.fillTriangle(x1, y1 - 2, x2, y2 - 2, x2, y2 + 2);
+				} else {
+					g.fillTriangle(x1 - 2, y1, x1 + 2, y1, x2 - 2, y2);
+					g.fillTriangle(x2 + 2, y2, x1 + 2, y1, x2 - 2, y2);
+				}
+			}
 		}
 	}
 
