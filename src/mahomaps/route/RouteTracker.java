5c2f4bdca35baab85c50cef8d150c8ae97e244c7
ansel <79257300125@ya.ru>
2023-06-04 15:43:55 +0300

    Limit extrapolation to 100%

--- src/mahomaps/route/RouteTracker.java
+++ src/mahomaps/route/RouteTracker.java
@@ -129,7 +129,9 @@ public class RouteTracker {
 	private void ProcessGeo() {
 		final long now = System.currentTimeMillis();
 		final double delta = (now - lastUpdateTime) / 1000d;
-		final double prg = (delta / timeDelta12) + 1d;
+		double prg = (delta / timeDelta12) + 1d;
+		if (prg > 2d)
+			prg = 2d;
 		double tlon = lerp(lon1, lon2, prg);
 		double tlat = lerp(lat1, lat2, prg);
 		if (delta < 1d) {
