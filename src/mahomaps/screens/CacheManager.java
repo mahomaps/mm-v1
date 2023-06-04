c2259110fc9ebbac4fc2e2d080567f93c70bc5bc
ansel <79257300125@ya.ru>
2023-06-04 17:25:04 +0300

    Add base of cache manager menu

---
+++ src/mahomaps/screens/CacheManager.java
@@ -0,0 +1,42 @@
+package mahomaps.screens;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.Item;
+import javax.microedition.lcdui.StringItem;
+
+import mahomaps.MahoMapsApp;
+import mahomaps.Settings;
+
+public class CacheManager extends Form implements CommandListener {
+
+	StringItem delAll = new StringItem(null, "Удалить все тайлы", Item.BUTTON);
+	private Command back = new Command("Назад", Command.BACK, 0);
+
+	public CacheManager() {
+		super("Управление кэшем");
+		addCommand(back);
+		setCommandListener(this);
+		append(new StringItem("Хранилище", getCacheType()));
+	}
+
+	private static String getCacheType() {
+		switch (Settings.cacheMode) {
+		case Settings.CACHE_DISABLED:
+			return "Не используется";
+		case Settings.CACHE_FS:
+			return "Файловая система";
+		case Settings.CACHE_RMS:
+			return "RMS";
+		default:
+			return null;
+		}
+	}
+
+	public void commandAction(Command c, Displayable d) {
+		if (c == back)
+			MahoMapsApp.BringMenu();
+	}
+}
