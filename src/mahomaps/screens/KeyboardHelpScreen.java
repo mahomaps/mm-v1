b9f009516959ed3d88f7c958c788613282fa130c
Shinovon <arman200711@gmail.com>
2023-06-06 05:29:49 +0500

    aboutscreen & keyboardhelpscreen fix

--- src/mahomaps/screens/KeyboardHelpScreen.java
+++ src/mahomaps/screens/KeyboardHelpScreen.java
@@ -4,6 +4,7 @@ import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.Item;
 import javax.microedition.lcdui.StringItem;
 
 import mahomaps.MahoMapsApp;
@@ -12,14 +13,19 @@ public class KeyboardHelpScreen extends Form implements CommandListener {
 
 	public KeyboardHelpScreen() {
 		super("Справка");
-		append(new StringItem("Навигация", "D-PAD"));
-		append(new StringItem("Масштаб", "1/* - приблизить\n3/# - отдалить"));
-		append(new StringItem("Поиск", "7"));
-		append(new StringItem("Геопозиция", "9"));
-		append(new StringItem("Меню приложения", "Левая софт-клавиша"));
-		append(new StringItem("Меню карты", "Правая софт-клавиша"));
+		add(new StringItem("Навигация", "D-PAD"));
+		add(new StringItem("Масштаб", "1/* - приблизить\n3/# - отдалить"));
+		add(new StringItem("Поиск", "7"));
+		add(new StringItem("Геопозиция", "9"));
+		add(new StringItem("Меню приложения", "Левая софт-клавиша"));
+		add(new StringItem("Меню карты", "Правая софт-клавиша"));
 		addCommand(MahoMapsApp.back);
 		setCommandListener(this);
+	}
+	
+	private void add(StringItem item) {
+		item.setLayout(Item.LAYOUT_NEWLINE_AFTER | Item.LAYOUT_NEWLINE_BEFORE);
+		append(item);
 	}
 
 	public void commandAction(Command c, Displayable d) {
