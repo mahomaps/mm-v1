1de03e5fbc4e018d0db7649bb7cd85b9c8284e20
ansel <79257300125@ya.ru>
2023-06-11 00:19:04 +0300

    Change buttons sizes based on input mode

--- src/mahomaps/ui/Button.java
+++ src/mahomaps/ui/Button.java
@@ -15,17 +15,21 @@ public class Button extends UIElement implements ITouchAcceptor {
 		this.text = text;
 		this.id = id;
 		this.handler = handler;
-		this.H = Font.getFont(0, 0, 8).getHeight() + margin * 4;
+		UpdateHeight();
 	}
 
 	public void Paint(Graphics g, int x, int y, int w, int h) {
 		RegisterForInput(this, x, y, w, h);
+		UpdateHeight();
 		g.setFont(Font.getFont(0, 0, 8));
 		if (hold) {
 			g.setColor(0xFC6155);
 			g.fillRoundRect(x + margin, y + margin, w - margin - margin, h - margin - margin, 10, 10);
 			g.setColor(0x343434);
-			g.fillRoundRect(x + margin * 2, y + margin * 2, w - margin * 4, h - margin * 4, 10, 10);
+			if (UIElement.touchInput)
+				g.fillRoundRect(x + margin * 2, y + margin * 2, w - margin * 4, h - margin * 4, 10, 10);
+			else
+				g.fillRoundRect(x + margin * 4, y + margin + 1, w - margin * 8, h - margin * 2 - 2, 10, 10);
 		} else {
 			g.setColor(0x343434);
 			g.fillRoundRect(x + margin, y + margin, w - margin - margin, h - margin - margin, 10, 10);
@@ -33,6 +37,10 @@ public class Button extends UIElement implements ITouchAcceptor {
 		int fh = g.getFont().getHeight();
 		g.setColor(-1);
 		g.drawString(text, x + w / 2, y + h / 2 - fh / 2, Graphics.TOP | Graphics.HCENTER);
+	}
+
+	private final void UpdateHeight() {
+		this.H = Font.getFont(0, 0, 8).getHeight() + margin * (UIElement.touchInput ? 4 : 2);
 	}
 
 	public void OnPress() {
