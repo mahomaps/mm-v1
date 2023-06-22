fe6ded5120b9fe7f50d9315e29d5093e33c6f1c5
Peter <p.timoshin2009@yandex.ru>
2023-06-22 23:15:34 +0300

    Add handler and fix typo
    1. Add handler for cache write fail
    2. Fix typo in word "response"

---
+++ src/mahomaps/overlays/CacheFailedOverlay.java
@@ -0,0 +1,43 @@
+package mahomaps.overlays;
+
+import java.util.Vector;
+
+import mahomaps.MahoMapsApp;
+import mahomaps.map.Geopoint;
+import mahomaps.ui.Button;
+import mahomaps.ui.ColumnsContainer;
+import mahomaps.ui.FillFlowContainer;
+import mahomaps.ui.IButtonHandler;
+import mahomaps.ui.SimpleText;
+import mahomaps.ui.UIElement;
+
+public class CacheFailedOverlay extends MapOverlay implements IButtonHandler {
+
+	public CacheFailedOverlay() {
+		content = new FillFlowContainer(new UIElement[] { new SimpleText("Caching failed!"),
+				new SimpleText("Please, check if your phone or SD memory is full."), new ColumnsContainer(new UIElement[] {
+						new Button(MahoMapsApp.text[38], 0, this) }) });
+	}
+
+	public String GetId() {
+		return "no_token";
+	}
+
+	public Vector GetPoints() {
+		return EMPTY_VECTOR;
+	}
+
+	public boolean OnPointTap(Geopoint p) {
+		return false;
+	}
+
+	public void OnButtonTap(UIElement sender, int uid) {
+		switch (uid) {
+		case 0:
+			Close();
+			break;
+		}
+
+	}
+
+}
