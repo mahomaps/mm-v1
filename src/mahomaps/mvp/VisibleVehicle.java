0764be4adfa22533ce78e866d0829c7898fa1973
Ansel Hayashi <symansel@outlook.com>
2026-08-09 03:33:49 +0300

    Add models for vehicle route

---
+++ src/mahomaps/mvp/VisibleVehicle.java
@@ -0,0 +1,27 @@
+package mahomaps.mvp;
+
+import cc.nnproject.json.JSONArray;
+import cc.nnproject.json.JSONObject;
+
+class VisibleVehicle {
+	public YGeometry[] routeSegments;
+	public int[] routeDurations;
+
+	public static VisibleVehicle decode(JSONObject j) {
+		VisibleVehicle v = new VisibleVehicle();
+		JSONArray rsArr = j.getArray("routeSegments");
+		JSONArray durArr = j.getArray("routeDurations");
+		v.routeSegments = new YGeometry[rsArr.size()];
+		v.routeDurations = new int[rsArr.size()];
+		for (int i = 0; i < rsArr.size(); i++) {
+			v.routeSegments[i] = YGeometry.decode(rsArr.getObject(i));
+			v.routeDurations[i] = durArr.getInt(i);
+		}
+		return v;
+	}
+
+	// === кеш, заполняется precalcVehicle ===
+	public int[] cumTime;          // длина = segments.length + 1
+	public double[][] cumDists;    // [seg][point] — кумулятивные длины в метрах
+	public boolean precalcDone;
+}
